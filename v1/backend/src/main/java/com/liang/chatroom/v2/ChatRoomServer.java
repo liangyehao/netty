68d2cfe0d6a2c8e5f2c72420d32e101498c5065d
liangyehao <1094311509@qq.com>
2020-12-07 00:13:16 +0800

    网页实现聊天室

---
+++ v1/backend/src/main/java/com/liang/chatroom/v2/ChatRoomServer.java
@@ -0,0 +1,58 @@
+package com.liang.chatroom.v2;
+
+import com.liang.chatroom.v1.server.ChatServerInitializer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+
+/**
+ * @author liangyehao
+ * @version 1.0
+ * @date 2020/12/6 20:21
+ * @content
+ */
+public class ChatRoomServer {
+
+    public void start(int port) {
+
+        EventLoopGroup boss = new NioEventLoopGroup();
+        EventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+
+            bootstrap.group(boss, worker)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childHandler(new ChannelInitializer<Channel>() {
+                        @Override
+                        protected void initChannel(Channel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast(new HttpServerCodec())
+                                    .addLast(new HttpObjectAggregator(1024*10))
+                                    .addLast(new WebSocketServerProtocolHandler("/"))
+                                    .addLast(new WebSocketChannelHandler());
+                        }
+                    });
+
+            ChannelFuture sync = bootstrap.bind(port).sync();
+            System.out.println("服务端启动成功,监听端口:"+port);
+
+            sync.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        ChatRoomServer chatRoomServer = new ChatRoomServer();
+        chatRoomServer.start(8080);
+    }
+}
